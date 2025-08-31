804e71c58da5b518b8215b5f2ad1faa81a2b946c
Krishna097 <gmkrishna097@gmail.com>
2025-08-31 07:54:49 +0000

    single nat

--- Ecomm/aws-iac/eks/main.tf
+++ Ecomm/aws-iac/eks/main.tf
@@ -2,14 +2,14 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  name_prefix            = local.name_prefix
-  vpc_cidr               = var.vpc_cidr
-  azs                    = local.azs
-  environment            = var.environment
-  project_name           = var.project_name
-  enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = true
+  name_prefix        = local.name_prefix
+  vpc_cidr           = var.vpc_cidr
+  azs                = local.azs
+  environment        = var.environment
+  project_name       = var.project_name
+  enable_nat_gateway = true
+  #single_nat_gateway     = false
+  one_nat_gateway_per_az = false
 
   tags = local.common_tags
 }
