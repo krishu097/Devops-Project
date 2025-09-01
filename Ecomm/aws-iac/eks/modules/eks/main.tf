5ca21ce40914792869bb5613e62b4aa1054dc561
Krishna097 <gmkrishna097@gmail.com>
2025-09-01 11:40:16 +0000

    disk size to 30g

--- Ecomm/aws-iac/eks/modules/eks/main.tf
+++ Ecomm/aws-iac/eks/modules/eks/main.tf
@@ -57,7 +57,7 @@ resource "aws_eks_node_group" "gmk-node-group" {
 
   instance_types = each.value.instance_types
   ami_type       = lookup(each.value, "ami_type", "AL2_x86_64")
-  disk_size      = lookup(each.value, "disk_size", 20)
+  disk_size      = lookup(each.value, "disk_size", 30)
   capacity_type  = lookup(each.value, "capacity_type", "ON_DEMAND")
 
 
