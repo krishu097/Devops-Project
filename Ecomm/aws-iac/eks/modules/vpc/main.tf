7c103645885475e592db64c295ae765ae0b493c7
Krishna <gmkrishna097@gmail.com>
2025-11-01 16:42:24 +0000

    fixing rds

--- Ecomm/aws-iac/eks/modules/vpc/main.tf
+++ Ecomm/aws-iac/eks/modules/vpc/main.tf
@@ -105,7 +105,7 @@ resource "aws_route_table_association" "private" {
 
 ###############################################################
 resource "aws_db_subnet_group" "db_subnet" {
-  name       = "${var.name_prefix}-db-subnet-group"
+  name       = lower("${var.name_prefix}-db-subnet-group")
   subnet_ids = aws_subnet.private[*].id
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-db-subnet-group"
