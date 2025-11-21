dbdbe726ebc8c1d2acb894692a2be84383cc6628
Krishna <gmkrishna097@gmail.com>
2025-11-21 08:30:06 +0000

    fixing outputs

--- Ecomm/aws-iac/eks/modules/eks/outputs.tf
+++ Ecomm/aws-iac/eks/modules/eks/outputs.tf
@@ -1,24 +1,24 @@
 output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
-  value       = var.deploy_secondary ? aws_eks_cluster.gmk-cluster[0].identity[0].oidc[0].issuer : null
+  value       = length(aws_eks_cluster.gmk-cluster) > 0 ? aws_eks_cluster.gmk-cluster[0].identity[0].oidc[0].issuer : null
 }
 
 output "cluster_id" {
   description = "EKS cluster ID"
-  value       = var.deploy_secondary ? aws_eks_cluster.gmk-cluster[0].id : null
+  value       = length(aws_eks_cluster.gmk-cluster) > 0 ? aws_eks_cluster.gmk-cluster[0].id : null
 }
 
 output "cluster_arn" {
   description = "EKS cluster ARN"
-  value       = var.deploy_secondary ? aws_eks_cluster.gmk-cluster[0].arn : null
+  value       = length(aws_eks_cluster.gmk-cluster) > 0 ? aws_eks_cluster.gmk-cluster[0].arn : null
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = var.deploy_secondary ? aws_eks_cluster.gmk-cluster[0].endpoint : null
+  value       = length(aws_eks_cluster.gmk-cluster) > 0 ? aws_eks_cluster.gmk-cluster[0].endpoint : null
 }
 
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = var.deploy_secondary ? aws_eks_cluster.gmk-cluster[0].certificate_authority[0].data : null
+  value       = length(aws_eks_cluster.gmk-cluster) > 0 ? aws_eks_cluster.gmk-cluster[0].certificate_authority[0].data : null
 }
