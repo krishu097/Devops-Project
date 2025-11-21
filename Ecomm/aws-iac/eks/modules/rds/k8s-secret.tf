3398a581546d184d977791c5edd4bb96b2e018fc
Krishna <gmkrishna097@gmail.com>
2025-11-21 08:26:24 +0000

    fixing secret

--- Ecomm/aws-iac/eks/modules/rds/k8s-secret.tf
+++ Ecomm/aws-iac/eks/modules/rds/k8s-secret.tf
@@ -1,18 +1,19 @@
-resource "kubernetes_secret" "rds_connection" {
-  count = var.create_k8s_secret ? 1 : 0
-  
-  metadata {
-    name      = "rds-connection"
-    namespace = "default"
-  }
-
-  data = {
-    endpoint = aws_db_instance.primary.endpoint
-    username = var.db_username
-    password = var.db_password
-    database = "businessproject"
-    jdbc_url = "jdbc:mysql://${aws_db_instance.primary.endpoint}/businessproject?useSSL=true&serverTimezone=UTC"
-  }
-
-  type = "Opaque"
-}
+# Kubernetes secret creation disabled - create manually after EKS is ready
+# resource "kubernetes_secret" "rds_connection" {
+#   count = var.create_k8s_secret ? 1 : 0
+#   
+#   metadata {
+#     name      = "rds-connection"
+#     namespace = "default"
+#   }
+#
+#   data = {
+#     endpoint = aws_db_instance.primary.endpoint
+#     username = var.db_username
+#     password = var.db_password
+#     database = "businessproject"
+#     jdbc_url = "jdbc:mysql://${aws_db_instance.primary.endpoint}/businessproject?useSSL=true&serverTimezone=UTC"
+#   }
+#
+#   type = "Opaque"
+# }
