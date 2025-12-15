56bd70f02a559dfe0fc89e78d8162bffa9cdf482
Krishna <gmkrishna097@gmail.com>
2025-12-15 12:19:19 +0000

    fixing service accounts

--- Ecomm/aws-iac/eks/providers.tf
+++ Ecomm/aws-iac/eks/providers.tf
@@ -8,47 +8,47 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "gmk-cluster" {
-  count = var.deploy_secondary ? 1 : 0
-  name  = module.eks.cluster_id
+  name = local.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "gmk-cluster" {
-  count = var.deploy_secondary ? 1 : 0
-  name  = module.eks.cluster_id
+  name = local.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
-  host                   = var.deploy_secondary ? data.aws_eks_cluster.gmk-cluster[0].endpoint : null
-  cluster_ca_certificate = var.deploy_secondary ? base64decode(data.aws_eks_cluster.gmk-cluster[0].certificate_authority[0].data) : null
-  token                  = var.deploy_secondary ? data.aws_eks_cluster_auth.gmk-cluster[0].token : null
+  host                   = data.aws_eks_cluster.gmk-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.gmk-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.gmk-cluster.token
 
   exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args = var.deploy_secondary ? [
+      args = [
         "eks",
         "get-token",
         "--cluster-name",
-        data.aws_eks_cluster.gmk-cluster[0].name
-      ] : []
+        data.aws_eks_cluster.gmk-cluster.name
+      ]
     }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = var.deploy_secondary ? data.aws_eks_cluster.gmk-cluster[0].endpoint : null
-    cluster_ca_certificate = var.deploy_secondary ? base64decode(data.aws_eks_cluster.gmk-cluster[0].certificate_authority[0].data) : null
-    token                  = var.deploy_secondary ? data.aws_eks_cluster_auth.gmk-cluster[0].token : null
+    host                   = data.aws_eks_cluster.gmk-cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.gmk-cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.gmk-cluster.token
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args = var.deploy_secondary ? [
+      args = [
         "eks",
         "get-token",
         "--cluster-name",
-        data.aws_eks_cluster.gmk-cluster[0].name
-      ] : []
+        data.aws_eks_cluster.gmk-cluster.name
+      ]
     }
   }
 }
